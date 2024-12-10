03f0c820d68ea23404b490ea500d2c96d702c90d
MAYURIRAMNATHSHINDE <mayurishinde24304@gmail.com>
2024-12-10 12:47:18 +0530

    Q.no 2

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-let age=21;
-function displayAge(){
-    console.log(age)
-}
-function changeAge(newage){
-    age=newage;
-    console.log(age)
+// let age=21;
+// function displayAge(){
+//     console.log(age)
+// }
+// function changeAge(newage){
+//     age=newage;
+//     console.log(age)
 
+// }
+// displayAge();
+// changeAge(32);
+// displayAge();
+
+
+// ***********************************  Q. no 2 *******************************//
+
+function createCounter(){
+    let counter=0;
+
+    return {
+        increment:function(){
+            counter++;
+            console.log(counter)
+    },
+    decrement:function (){
+        counter--;
+        console.log(counter)
+    }
+        }
 }
-displayAge();
-changeAge(32);
-displayAge();
+const counter = createCounter();
+counter.increment(); // Output: Current count: 1
+counter.increment(); // Output: Current count: 2
+counter.increment();
+counter.decrement(); // Output: Current count: 1
+counter.increment();
+console.log("New Counter ")
+const counter2 = createCounter();
+counter2.increment(); // Output: Current count: 1
+counter2.increment();
